f49ac8e79bd2aa0be7c38104a9de20635db9929d
tuando <tuandomanh2004@gmail.com>
2026-05-13 13:06:27 +0700

    ref: match system

--- Assets/Scripts/Match.cs
+++ Assets/Scripts/Match.cs
@@ -1,50 +1,44 @@
 using System;
 using System.Numerics;
+using NUnit.Framework;
 using Unity.VisualScripting.Antlr3.Runtime;
 using UnityEngine;
 using UnityEngine.InputSystem.LowLevel;
+using System.Collections.Generic ; 
 
 public class Match : MonoBehaviour
 {
     [SerializeField] private int requiredAdjacentMatches = 2;
-    [SerializeField] private bool hasMatch = false ; 
+    [SerializeField] private bool hasMatch = false;
     void Start()
     {
 
     }
-
-    // Update is called once per frame
     void Update()
     {
 
     }
     public bool HasMatch(Gem[,] board, Vector2Int gemAPos, Vector2Int gemBPos)
     {
-        //int gemCounter = 1;
-        if (IsVerticalSwap(gemAPos, gemBPos))
-        {
-           return HasVerticalMatch(board , gemAPos , gemBPos) || HasHorizontalMatch(board ,gemAPos, gemBPos);
-            
-        }
-        return false ; 
+        return HasMatchAt(board, gemAPos) | HasMatchAt(board, gemBPos);
     }
-    public bool HasHorizontalMatch(Gem[,] board, Vector2Int gemAPos ,Vector2Int gemBPos){
-       return true ; 
-    }
-    public bool HasVerticalMatch(Gem[,] board, Vector2Int gemAPos, Vector2Int gemBPos)
+    public bool HasMatchAt(Gem[,] board, Vector2Int pos)
     {
-        int gemCounter = 1;
-        int horizontalIndex = gemAPos.y;
-        int verticalStart = Math.Max(Math.Min(gemAPos.x, gemBPos.x) - requiredAdjacentMatches, 0);
-        int verticalEnd = Math.Min(Math.Max(gemAPos.x, gemBPos.x) + requiredAdjacentMatches, board.GetLength(0) - 1);
-        int startPointer = verticalStart, endPointer = verticalStart + 1;
-      //  Debug.Log($"{verticalStart} , {verticalEnd} ");
-        while (endPointer <= verticalEnd)
+        return HasHorizontalMatch(board, pos) | HasVerticalMatch(board, pos);
+    }
+    public bool HasHorizontalMatch(Gem[,] board, Vector2Int gemPos)
+    {
+        int gemCounter = 1 ; 
+        int verticalIndex = gemPos.x ;
+        int boardWidth = board.GetLength(1) - 1 ; 
+        int start = 0 , end = start + 1 ; 
+        while(end <= boardWidth)
         {
-           
-            if (board[endPointer, horizontalIndex].Type != board[startPointer, horizontalIndex].Type)
+            Gem currentGem = board[verticalIndex, end];
+            Gem gemToMatch = board[verticalIndex,start];
+            if (currentGem.Type != gemToMatch.Type)
             {
-                startPointer = endPointer;
+                start = end;
                 gemCounter = 1;
             }
             else
@@ -52,23 +46,42 @@ public class Match : MonoBehaviour
                 gemCounter++;
                 if (gemCounter >= 3)
                 {
-                    hasMatch = true;
-                    Debug.Log("MATCH!!!") ; 
-                    break ; 
+                    Debug.Log("MATCH!!!");
+                    Debug.Log($"board[{verticalIndex},{start}] -> board[{verticalIndex},{end}]") ; 
+                    return true ; 
                 }
             }
-            endPointer++;
-        }
-       
-        for(int i = 0 ; i < board.GetLength(0) ; i++)
-        {
-            Debug.Log($"pos[{i},{horizontalIndex}] :  {board[i,horizontalIndex].name} , {board[i, horizontalIndex].BoardPosition}");
-        }
-      
-        return hasMatch;
+            end++ ; 
+        }  
+        return false ; 
     }
-    public bool IsVerticalSwap(Vector2Int gemAPos, Vector2Int gemBPos)
+    public bool HasVerticalMatch(Gem[,] board, Vector2Int gemPos)
     {
-        return Math.Abs(gemAPos.x - gemBPos.x) == 1 && gemAPos.y == gemBPos.y;
+        int gemCounter = 1;
+        int horizontalIndex = gemPos.y;
+        int boardHeight = board.GetLength(0) - 1;
+        int start = 0, end = start + 1;
+        while (end <= boardHeight)
+        {
+            Gem currentGem = board[end, horizontalIndex];
+            Gem gemToMatch = board[start, horizontalIndex];
+            if (currentGem.Type != gemToMatch.Type)
+            {
+                start = end;
+                gemCounter = 1;
+            }
+            else
+            {
+                gemCounter++;
+                if (gemCounter >= 3)
+                {
+                    Debug.Log("MATCH!!!");
+                    Debug.Log($"board[{start},{horizontalIndex}] -> board[{end},{horizontalIndex}]") ; 
+                    return true ; 
+                }
+            }
+            end++ ; 
+        }
+        return false ;  
     }
 }
